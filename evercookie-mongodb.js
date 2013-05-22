b0df4c5a6ff54352e395a14f3491f8e39dc2c2eb
Kevin Kirchner <kevin@classyllama.com>
2013-05-22 08:22:33 -0500

    making it into a jquery plugin

--- evercookie-mongodb.js
+++ evercookie-mongodb.js
@@ -1,70 +1,98 @@
 
 (function($, window, undefined){
 
-    var APIKEY = '?apiKey=50f47f5fe4b0b9deb2482c6b';
-    var MONGODB = 'evercookie-mongodb';
-    var MONGOCOLLECTION = 'userData';
-    var MONGOURL = 'https://api.mongolab.com/api/1/databases/'+MONGODB+'/collections/'+MONGOCOLLECTION+'/';
-    var EC = new evercookie();
-    var EID;
-    var MONGODATA;    
-    
-    function init() {
-        var create = 0;
-        EC.get("uid", function(best, all) {
-            if (typeof best == "undefined") {
-                create = 1;
-            } else {
-                EID = best;
-            }
-            getMongoData(create);
-        }, 0);
+    var EverMongo = function(element, options) {
+        this.init(element, options);
     };
-    
-    function getMongoData(create) {
-        if (MONGODATA) return MONGODATA;
+
+    EverMongo.prototype = {
+        constructor: EverMongo,
+        init: function(element, options) {
+            var that = this;
+            that.options = $.extend({}, $.fn.everMongo.defaults, options);
+            that.ec = new evercookie();
+            that.evercookieID = null;
+            that.mongoData = null;
+            that.mongoFullUrl = that.options.mongoUrl + 'databases/' + that.options.db + '/collections/' + that.options.collection + '/';
+            that.run();
+        },
+        run: function(){
+            var that = this;
+            var create = 0;
+            that.ec.get("uid", function(best, all) {
+                if (typeof best == "undefined") {
+                    create = 1;
+                } else {
+                    that.evercookieID = best;
+                }
+                that.getMongoData(create);
+            }, 0);
+        },
+        getMongoData: function(create) {
+            var that = this;
+            if (that.mongoData) return that.mongoData;
         
-        if (create) {
-            newMongoDoc(setMongoData)
-        } else {
-            // use EID to get info
-            getMongoDataByEID(function(data){
-                setMongoData(data);
+            if (create) {
+                that.newMongoDoc( that.setMongoData );
+            } else {
+                // use EID to get info
+                that.getMongoDataByEID( function(data) {
+                    that.setMongoData(data);
+                });
+            }
+        },
+        newMongoDoc: function(callback) {
+            var that = this;
+            $.ajax({ 
+                url: that.mongoFullUrl + '?apiKey=' + that.options.apiKey,
+                data: JSON.stringify( that.options.defaultData ),
+                type: "POST",
+                contentType: "application/json",
+                success: callback
+            });
+        },
+        setMongoData: function(mongoData) {
+            var that = this;
+            that.mongoData = mongoData;
+            if (!that.evercookieID) {
+                that.evercookieID = mongoData._id.$oid;
+                that.ec.set("eid", that.evercookieID);
+            }
+            if (typeof that.options.completeCallback == "function") {
+                that.options.completeCallback( that.mongoData, that.evercookieID );
+            }
+        },
+        getMongoDataByEID: function(callback) {
+            var that = this;
+            $.ajax({ 
+                url: that.mongoFullUrl + that.evercookieID + '?apiKey=' + that.options.apiKey,
+                type: "GET",
+                contentType: "application/json",
+                success: callback
             });
         }
-    }
-    
-    function getMongoDataByEID(callback) {
-        $.ajax({ 
-            url: MONGOURL + EID + APIKEY,
-            type: "GET",
-            contentType: "application/json",
-            success: callback
+    };
+
+
+    // define plugin
+    $.fn.everMongo = function( options ) {
+        return this.each( function () {
+            new EverMongo(this, options);
         });
-    }
-    
-    function setMongoData(data) {
-        MONGODATA = data;
-        if (typeof EID == "undefined") {
-            EID = data._id.$oid;
-            EC.set("uid",EID);
+    };
+
+    $.fn.everMongo.constructor = EverMongo;
+
+    // define defaults
+    $.fn.everMongo.defaults = {
+        apiKey: '',
+        db: '',
+        collection: '',
+        defaultData: { "testing": "1,2" },
+        mongoUrl: 'https://api.mongolab.com/api/1/',
+        completeCallback: function(mongoData, evercookieID){
+            console.log(mongoData, evercookieID);
         }
-        if (typeof evercookieMongoCallback == "function") {
-            evercookieMongoCallback( MONGODATA, EID )
-        }
-    }
-    
-    function newMongoDoc(callback) {
-        $.ajax({ 
-            url: MONGOURL + APIKEY,
-            data: JSON.stringify( { "yup" : "ok" } ),
-            type: "POST",
-            contentType: "application/json",
-            success: callback
-        });
-    }
-    
-    // start the maddness
-    init();
+    };
     
 })(jQuery, this);
